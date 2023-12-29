a1a57a5f90e8321408d54c3ccab8591d7c1e9710
ramziddin-frontend <107789041+ramziddin-frontend@users.noreply.github.com>
2023-12-29 19:03:08 +0500

    Add user, product route and errorHandle, notFound,

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 const express = require('express')
-const app = express()
 const connectDB = require('./config/connectDB');
-const dotenv  = require('dotenv').config()
+const app = express()
+const bodyParser = require('body-parser');
+const authRouter = require('./routes/authRoute');
+const productRoute = require('./routes/productRoute')
+const { notFound, errorHandler } = require('./middlewares/errorHandler');
+const dotenv  = require('dotenv').config();
+const cookieParser = require("cookie-parser");
+const morgan = require('morgan')
 
 const port = process.env.PORT || 4000
+
 connectDB()
 
+app.use(morgan("dev"))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser())
+
+app.use('/api/user', authRouter);
+app.use('/api/product', productRoute)
 app.use("/", (req, res) => {
     res.send("hello from serve")
-})
-app.get('/', (req, res) => res.send('Hello World!'))
+});
+
+app.use(errorHandler);
+app.use(notFound);
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
