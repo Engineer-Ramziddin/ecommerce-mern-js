a31aa349fd096d7d74151fd510cfd4813951989a
ramziddin-frontend <107789041+ramziddin-frontend@users.noreply.github.com>
2023-12-29 20:43:36 +0500

    Add the admin middleware to the product route

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,10 +1,15 @@
 const express = require('express');
-const { createProduct, getProduct, getAllProduct, updateProduct } = require('../controller/productController');
+const { createProduct, getProduct, getAllProduct, updateProduct, deleteProduct } = require('../controller/productController');
 const router = express.Router();
+const { authMiddleware, isAdmin } = require('../middlewares/authmiddlewares');
 
-router.post('/create', createProduct);
-router.put('/update/:id', updateProduct)
-router.get('/:id', getProduct);
+
+
 router.get('/', getAllProduct);
+router.get('/:id', getProduct);
+router.post('/create', authMiddleware, isAdmin, createProduct);
+router.put('/update/:id', authMiddleware, isAdmin, updateProduct);
+router.delete('/:id', authMiddleware, isAdmin, deleteProduct);
+
 
 module.exports = router;
