08646256d27474b553db3484ffafa822c2074cf2
ramziddin-frontend <107789041+ramziddin-frontend@users.noreply.github.com>
2023-12-30 10:59:57 +0500

    add updatePassword, forgotPassword, resetPassword,

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { createUser, loginUser, getAllUser, getOneUser, deleteaUser, updatedUser, blockUser, unBlockUser, handlerRefreshToken, logOut } = require('../controller/userController');
+const { createUser, loginUser, getAllUser, getOneUser, deleteaUser, updatedUser, blockUser, unBlockUser, handlerRefreshToken, logOut, updatePassword, forgotPassword, resetPassword } = require('../controller/userController');
 const { authMiddleware, isAdmin } = require('../middlewares/authmiddlewares');
 const router = express.Router();
 
@@ -12,5 +12,9 @@ router.delete('/delete/:id', deleteaUser)
 router.put('/block/:userId', blockUser);
 router.put('/unblock/:userId', unBlockUser);
 router.get('/refreshtoken', handlerRefreshToken);
-router.get('/logout', logOut)
+router.get('/logout', logOut);
+
+router.put('/updatepassword', authMiddleware, updatePassword);
+router.post('/forgot-password-token', forgotPassword);
+router.put('/reset-password/:token', resetPassword);
 module.exports = router;
