af3297887475a863686b3590e846249be19e891e
ramziddin-frontend <107789041+ramziddin-frontend@users.noreply.github.com>
2023-12-30 20:47:12 +0500

    Add restfull blog category

---
+++ controller/blogCatCtrl.js
@@ -0,0 +1,66 @@
+const expressAsyncHandler = require("express-async-handler");
+const Category = require("../models/blogCatModel");
+const validateMongoDbId = require("../utils/validateMongodbId");
+
+const createCategory = expressAsyncHandler(async (req, res) => {
+  try {
+    const existingCategory = await Category.findOne({ title: req.body.title });
+
+    if (existingCategory) {
+      return res.status(400).json({ message: 'Category already exists' });
+    }
+
+    // Category does not exist, create a new one
+    const newCategory = await Category.create(req.body);
+    res.json(newCategory);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+const updateCategory = expressAsyncHandler(async (req, res) => {
+  const { id } = req.params;
+  validateMongoDbId(id);
+  try {
+    const updatedCategory = await Category.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
+    res.json(updatedCategory);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+const deleteCategory = expressAsyncHandler(async (req, res) => {
+  const { id } = req.params;
+  validateMongoDbId(id);
+  try {
+    const deletedCategory = await Category.findByIdAndDelete(id);
+    res.json(deletedCategory);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+const getCategory = expressAsyncHandler(async (req, res) => {
+  const { id } = req.params;
+  validateMongoDbId(id);
+  try {
+    const getaCategory = await Category.findById(id);
+    res.json(getaCategory);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+const getallCategory = expressAsyncHandler(async (req, res) => {
+  try {
+    const getallCategory = await Category.find();
+    res.json(getallCategory);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+module.exports = {
+  createCategory,
+  updateCategory,
+  deleteCategory,
+  getCategory,
+  getallCategory,
+};
