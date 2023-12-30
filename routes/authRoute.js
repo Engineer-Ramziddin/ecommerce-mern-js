0f332474f2641e4f812adf97bb53154045ad02c3
ramziddin-frontend <107789041+ramziddin-frontend@users.noreply.github.com>
2023-12-30 22:11:32 +0500

    add     userCart,     getUserCart,     emptyCart,     applyCoupon,   createOrder,   getOrders,   updateOrderStatus,   getAllOrders,   getOrderByUserId, routes

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const { createUser, loginUser, getAllUser, getOneUser, deleteaUser, updatedUser, blockUser, unBlockUser, handlerRefreshToken, logOut, updatePassword, forgotPassword, resetPassword, saveAddress, getWishlist } = require('../controller/userController');
+const { createUser, loginUser, getAllUser, getOneUser, deleteaUser, updatedUser, blockUser, unBlockUser, handlerRefreshToken, logOut, updatePassword, forgotPassword, resetPassword, saveAddress, getWishlist, emptyCart, getUserCart, userCart, updateOrderStatus, getOrders, getAllOrders, getOrderByUserId, applyCoupon, createOrder } = require('../controller/userController');
 const { authMiddleware, isAdmin } = require('../middlewares/authmiddlewares');
 const router = express.Router();
 
+router.post('/cart', authMiddleware, userCart);
 router.post('/register', createUser);
 router.post('/login', loginUser);
+
 router.get('/all', authMiddleware, isAdmin, getAllUser);
 router.get('/get/:id', getOneUser);
 router.put('/update/:id', authMiddleware, updatedUser);
@@ -21,4 +23,15 @@ router.get('/logout', logOut);
 router.put('/updatepassword', authMiddleware, updatePassword);
 router.post('/forgot-password-token', forgotPassword);
 router.put('/reset-password/:token', resetPassword);
+
+router.get("/cart", authMiddleware, getUserCart);
+router.delete("/empty-cart", authMiddleware, emptyCart);
+
+router.post("/cart/cash-order", authMiddleware, createOrder);
+router.post("/cart/applycoupon", authMiddleware, applyCoupon);
+router.get("/get-orders", authMiddleware, getOrders);
+router.get("/getallorders", authMiddleware, isAdmin, getAllOrders);
+router.post("/getorderbyuser/:id", authMiddleware, isAdmin, getOrderByUserId);
+router.put( "/order/update-order/:id", authMiddleware, isAdmin, updateOrderStatus);
+
 module.exports = router;
