36dcbb46deb7f371e13311b2de69c48dae46b3da
ramziddin-frontend <107789041+ramziddin-frontend@users.noreply.github.com>
2024-01-02 22:59:01 +0500

    demo img

--- ui/src/pages/Home.js
+++ ui/src/pages/Home.js
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+      <div>
+        Home
+      </div>
+      {/*<img src={require('assets/images/image-1.jpg')} />*/}
+    </>
   )
 }
 
