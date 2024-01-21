6f3d4d2cf5f50442c6d77a19e6ebd5c3d25b8914
ramziddin-frontend <107789041+ramziddin-frontend@users.noreply.github.com>
2024-01-21 21:03:33 +0500

    Create CompareProduct.js

---
+++ ui/src/pages/CompareProduct.js
@@ -0,0 +1,202 @@
+import React from 'react';
+import Meta from "../components/Meta";
+import BreadCrumb from "../components/BreadCrumb";
+import {RxCross1} from "react-icons/rx";
+import Color from "../components/Color";
+
+const CompareProduct = () => {
+    return (
+        <>
+            <Meta title={"Compare Product"}/>
+            <BreadCrumb title={"Compare Product"}/>
+            <div className="compare-product-wrapper home-wrapper-2 py-5">
+                <div className="container-xxl">
+                    <div className="row">
+                        <div className="col-3">
+                            <div className="compare-product-card position-relative">
+                                <RxCross1 className="position-absolute cross img-fluid" size={35}/>
+                                <div className="product-card-image">
+                                    <img src="https://m.media-amazon.com/images/I/71ulah9iIwL._AC_SL1500_.jpg"
+                                         alt="image"/>
+                                </div>
+                                <div className="compare-product-details">
+                                    <h5 className="title">
+                                        Apple Watch Series 8 [GPS 41mm] Smart Watch w/Midnight Aluminum Case with
+                                        Midnight Sport Band - M/L. Fitness Tracker, Blood Oxygen & ECG Apps, Always-On
+                                        Retina Display, Water Resistant
+                                    </h5>
+                                    <h6 className="price">
+                                        $364.89
+                                    </h6>
+
+                                    <div>
+                                        <div className="product-detail">
+                                            <h5>Brand:</h5>
+                                            <p>Havels</p>
+                                        </div>
+                                        <div className="product-detail">
+                                            <h5>Type:</h5>
+                                            <p>Watch</p>
+                                        </div>
+                                        <div className="product-detail">
+                                            <h5>Availablity:</h5>
+                                            <p>In stock</p>
+                                        </div>
+                                        <div className="product-detail">
+                                            <h5 className="my-1">Color:</h5>
+                                            <Color/>
+                                        </div>
+                                        <div className="product-detail">
+                                            <h5>Size:</h5>
+                                            <div className="d-flex gap-10">
+                                                <p>S</p>
+                                                <p>M</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-3">
+                            <div className="compare-product-card position-relative">
+                                <RxCross1 className="position-absolute cross img-fluid" size={35}/>
+                                <div className="product-card-image">
+                                    <img src="https://m.media-amazon.com/images/I/71ulah9iIwL._AC_SL1500_.jpg"
+                                         alt="image"/>
+                                </div>
+                                <div className="compare-product-details">
+                                    <h5 className="title">
+                                        Apple Watch Series 8 [GPS 41mm] Smart Watch w/Midnight Aluminum Case with
+                                        Midnight Sport Band - M/L. Fitness Tracker, Blood Oxygen & ECG Apps, Always-On
+                                        Retina Display, Water Resistant
+                                    </h5>
+                                    <h6 className="price">
+                                        $364.89
+                                    </h6>
+
+                                    <div>
+                                        <div className="product-detail">
+                                            <h5>Brand:</h5>
+                                            <p>Havels</p>
+                                        </div>
+                                        <div className="product-detail">
+                                            <h5>Type:</h5>
+                                            <p>Watch</p>
+                                        </div>
+                                        <div className="product-detail">
+                                            <h5>Availablity:</h5>
+                                            <p>In stock</p>
+                                        </div>
+                                        <div className="product-detail">
+                                            <h5 className="my-1">Color:</h5>
+                                            <Color/>
+                                        </div>
+                                        <div className="product-detail">
+                                            <h5>Size:</h5>
+                                            <div className="d-flex gap-10">
+                                                <p>S</p>
+                                                <p>M</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-3">
+                            <div className="compare-product-card position-relative">
+                                <RxCross1 className="position-absolute cross img-fluid" size={35}/>
+                                <div className="product-card-image">
+                                    <img src="https://m.media-amazon.com/images/I/71ulah9iIwL._AC_SL1500_.jpg"
+                                         alt="image"/>
+                                </div>
+                                <div className="compare-product-details">
+                                    <h5 className="title">
+                                        Apple Watch Series 8 [GPS 41mm] Smart Watch w/Midnight Aluminum Case with
+                                        Midnight Sport Band - M/L. Fitness Tracker, Blood Oxygen & ECG Apps, Always-On
+                                        Retina Display, Water Resistant
+                                    </h5>
+                                    <h6 className="price">
+                                        $364.89
+                                    </h6>
+
+                                    <div>
+                                        <div className="product-detail">
+                                            <h5>Brand:</h5>
+                                            <p>Havels</p>
+                                        </div>
+                                        <div className="product-detail">
+                                            <h5>Type:</h5>
+                                            <p>Watch</p>
+                                        </div>
+                                        <div className="product-detail">
+                                            <h5>Availablity:</h5>
+                                            <p>In stock</p>
+                                        </div>
+                                        <div className="product-detail">
+                                            <h5 className="my-1">Color:</h5>
+                                            <Color/>
+                                        </div>
+                                        <div className="product-detail">
+                                            <h5>Size:</h5>
+                                            <div className="d-flex gap-10">
+                                                <p>S</p>
+                                                <p>M</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-3">
+                            <div className="compare-product-card position-relative">
+                                <RxCross1 className="position-absolute cross img-fluid" size={35}/>
+                                <div className="product-card-image">
+                                    <img src="https://m.media-amazon.com/images/I/71ulah9iIwL._AC_SL1500_.jpg"
+                                         alt="image"/>
+                                </div>
+                                <div className="compare-product-details">
+                                    <h5 className="title">
+                                        Apple Watch Series 8 [GPS 41mm] Smart Watch w/Midnight Aluminum Case with
+                                        Midnight Sport Band - M/L. Fitness Tracker, Blood Oxygen & ECG Apps, Always-On
+                                        Retina Display, Water Resistant
+                                    </h5>
+                                    <h6 className="price">
+                                        $364.89
+                                    </h6>
+
+                                    <div>
+                                        <div className="product-detail">
+                                            <h5>Brand:</h5>
+                                            <p>Havels</p>
+                                        </div>
+                                        <div className="product-detail">
+                                            <h5>Type:</h5>
+                                            <p>Watch</p>
+                                        </div>
+                                        <div className="product-detail">
+                                            <h5>Availablity:</h5>
+                                            <p>In stock</p>
+                                        </div>
+                                        <div className="product-detail">
+                                            <h5 className="my-1">Color:</h5>
+                                            <Color/>
+                                        </div>
+                                        <div className="product-detail">
+                                            <h5>Size:</h5>
+                                            <div className="d-flex gap-10">
+                                                <p>S</p>
+                                                <p>M</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default CompareProduct;
