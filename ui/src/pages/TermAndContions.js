8909bc7c91a969ef47785481c87114d689da2eed
ramziddin-frontend <107789041+ramziddin-frontend@users.noreply.github.com>
2024-01-21 21:05:31 +0500

    Create TermAndContions.js

---
+++ ui/src/pages/TermAndContions.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Meta from "../components/Meta";
+import BreadCrumb from "../components/BreadCrumb";
+
+const TermAndContions = () => {
+    return (
+        <>
+            <Meta title={"Term and Contions"}/>
+            <BreadCrumb title={"Term and Contions"}/>
+            <div className="policy-wrapper home-wrapper-2 py-5">
+                <div className="container-xxl">
+                    <div className="row">
+                        <div className="col-12">
+                            <div className="policy">
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default TermAndContions;
