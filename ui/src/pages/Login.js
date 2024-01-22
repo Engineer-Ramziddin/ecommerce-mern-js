3e4e6bb164f47a91f6101714550ebb357e2eccfc
ramziddin-frontend <107789041+ramziddin-frontend@users.noreply.github.com>
2024-01-22 07:37:30 +0500

    edit input to Custom input

--- ui/src/pages/Login.js
+++ ui/src/pages/Login.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Meta from "../components/Meta";
 import BreadCrumb from "../components/BreadCrumb";
 import {Link} from "react-router-dom";
+import CustomInput from "../components/CustomInput";
 
 const Login = () => {
     return (
@@ -16,19 +17,17 @@ const Login = () => {
                                 <h3>Login</h3>
                                 <form className="form" action="">
                                     <div>
-                                        <input
+                                        <CustomInput
                                             type="email"
                                             name="email"
                                             placeholder="Email"
-                                            className="form-control"
                                         />
                                     </div>
                                     <div>
-                                        <input
+                                        <CustomInput
                                             type="password"
                                             name="password"
                                             placeholder="Password"
-                                            className="form-control"
                                         />
                                     </div>
                                     <div>
