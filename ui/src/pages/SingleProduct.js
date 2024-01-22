989779b1f02230e3fc7562bd3dadbd2e624d0366
ramziddin-frontend <107789041+ramziddin-frontend@users.noreply.github.com>
2024-01-22 07:38:33 +0500

    add react image zoom

--- ui/src/pages/SingleProduct.js
+++ ui/src/pages/SingleProduct.js
@@ -3,24 +3,167 @@ import BreadCrumb from "../components/BreadCrumb";
 import Meta from "../components/Meta";
 import ProductCard from "../components/ProductCard";
 import ReactStars from "react-rating-stars-component";
-
+import nout from '../assets/images/nout-1-1.jpg';
+import ReactImageMagnify from 'react-image-magnify';
+import Color from "../components/Color";
+import {MdCompareArrows} from "react-icons/md";
+import {AiOutlineHeart} from "react-icons/ai";
 
 const SingleProduct = () => {
-
+    const zoomImg = nout;
     const [orderedProduct, setOrderedProduct] = useState(true);
+    const copyToClipboard = (text) => {
+        console.log('text', text)
+        text = `http://localhost:3000${text}`
+        var textField = document.createElement('textarea')
+        textField.innerText = text
+        document.body.appendChild(textField)
+        textField.select()
+        document.execCommand('copy')
+        textField.remove()
+    }
     return (
         <>
             <Meta title={"Product Name"}/>
             <BreadCrumb title={"Product Name"}/>
             <div className="main-product-wrapper home-wrapper-2 py-5">
                 <div className="container-xxl">
-                    <div className="row">
+                    <div className="row bg-white">
                         <div className="col-6">
                             <div className="main-product-image">
-
+                                <div>
+                                    <ReactImageMagnify {...{
+                                        smallImage: {
+                                            alt: 'Wristwatch by Ted Baker London',
+                                            isFluidWidth: true,
+                                            src: zoomImg
+                                        },
+                                        largeImage: {
+                                            src: zoomImg,
+                                            width: 1200,
+                                            height: 1800
+                                        },
+                                        className: "ReactImageMagnify"
+                                    }} />
+                                </div>
+                            </div>
+                            <div className="other-product-images d-flex flex-wrap gap-15">
+                                <div><img className="img-fluid" src={nout} alt=""/></div>
+                                <div><img className="img-fluid" src={nout} alt=""/></div>
+                                <div><img className="img-fluid" src={nout} alt=""/></div>
+                                <div><img className="img-fluid" src={nout} alt=""/></div>
                             </div>
                         </div>
                         <div className="col-6">
+                            <div className="main-product-details">
+                                <div className="border-bottom">
+                                    <h3 className="title">
+                                        Kids headphones bulk 10 pack multi colored for students
+                                    </h3>
+                                </div>
+                                <div className="border-bottom">
+                                    <p className="price">$ 100</p>
+                                    <div className="d-flex align-items-center gap-10">
+                                        <ReactStars
+                                            count={5}
+                                            size={24}
+                                            value={3}
+                                            edit={false}
+                                            activeColor="#ffd700"
+                                        />
+                                        <p className="mb-0">( 2 Reviews )</p>
+                                    </div>
+                                    <a href="#review">Write a Review</a>
+                                </div>
+                                <div className="border-bottom py-3">
+                                    <div className="d-flex align-items-center gap-10 my-2">
+                                        <h3 className="product-heading">Type: </h3>
+                                        <p className="product-data">Watch</p>
+                                    </div>
+                                    <div className="d-flex align-items-center gap-10 my-2">
+                                        <h3 className="product-heading">Brand: </h3>
+                                        <p className="product-data">Havells</p>
+                                    </div>
+                                    <div className="d-flex align-items-center gap-10 my-2">
+                                        <h3 className="product-heading">Category: </h3>
+                                        <p className="product-data">Watch</p>
+                                    </div>
+                                    <div className="d-flex align-items-center gap-10 my-2">
+                                        <h3 className="product-heading">Tags: </h3>
+                                        <p className="product-data">Watch</p>
+                                    </div>
+                                    <div className="d-flex align-items-center gap-10 my-2">
+                                        <h3 className="product-heading">Avialablity: </h3>
+                                        <p className="product-data">In stock</p>
+                                    </div>
+                                    <div className="d-flex align-items-center gap-10 my-2">
+                                        <h3 className="product-heading">Size: </h3>
+                                        <div className="d-flex flex-wrap gap-15">
+                                            <span
+                                                className="badge text-dark border-1 bg-white border-secondary border">S</span>
+                                            <span
+                                                className="badge text-dark border-1 bg-white border-secondary border">M</span>
+                                            <span
+                                                className="badge text-dark border-1 bg-white border-secondary border">XL</span>
+                                            <span
+                                                className="badge text-dark border-1 bg-white border-secondary border">XXL</span>
+                                        </div>
+                                    </div>
+                                    <div className="d-flex align-items-center gap-10 my-2">
+                                        <h3 className="product-heading">Color: </h3>
+                                        <Color/>
+                                    </div>
+                                    <div className="d-flex flex-row mt-2 mb-3">
+                                        <h3 className="product-heading">Quantity: </h3>
+                                        <div>
+                                            <input
+                                                type="number"
+                                                name=""
+                                                id=""
+                                                min="1"
+                                                max="10"
+                                                className="form-control mx-2"
+                                                style={{width: "70px"}}
+                                            />
+                                        </div>
+                                        <div className="d-flex justify-content-center gap-30 align-items-center ms-2">
+                                            <button type="submit" className="button">Add to Card</button>
+                                            <button className="button">Buy Now</button>
+                                        </div>
+                                    </div>
+                                    <div className="d-flex align-items-center gap-10 my-2">
+                                        <div>
+                                            <a href="">
+                                                <MdCompareArrows size={35}/> Add to Compare
+                                            </a>
+                                        </div>
+                                        <div>
+                                            <a href="">
+                                                <AiOutlineHeart size={35}/> Add to Wishlist
+                                            </a>
+                                        </div>
+                                    </div>
+                                    <div className="d-flex align-items-center gap-10 my-2">
+                                        <h3 className="product-heading">Shipping & Returns: </h3>
+                                        <p className="product-data">
+                                            Free shipping and returns avialable on all orders! <br/>{""}
+                                            We ship all US domestic orders within{""}
+                                            <b>5-10 business days!</b>
+                                        </p>
+                                    </div>
+                                    <div className="d-flex align-items-center gap-10 my-2">
+                                        <h3 className="product-heading">Copy Product Link: </h3>
+                                        <a
+                                            href="javascipt:void(0)"
+                                            onClick={()=>{
+                                                copyToClipboard(nout)
+                                            }}
+                                        >
+                                            Copy Product Link
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -57,7 +200,7 @@ const SingleProduct = () => {
                 </div>
             </div>
 
-            <div className="reviews-wrapper py-5 home-wrapper-2">
+            <section id="review" className="reviews-wrapper py-5 home-wrapper-2">
                 <div className="container-xxl">
                     <div className="row">
                         <div className="col-12">
@@ -102,16 +245,16 @@ const SingleProduct = () => {
                                                 />
                                             </div>
                                             <div>
-                                                 <textarea
-                                                     name=""
-                                                     id=""
-                                                     className="w-100 form-control"
-                                                     cols="30"
-                                                     rows="10"
-                                                     placeholder="Comments"
-                                                 >
+                                                <textarea
+                                                    name=""
+                                                    id=""
+                                                    className="w-100 form-control"
+                                                    cols="30"
+                                                    rows="10"
+                                                    placeholder="Comments"
+                                                >
 
-                                    </textarea>
+                                                </textarea>
                                             </div>
                                             <div className="d-flex justify-content-end">
                                                 <button type="submit" className="button">Submit Review</button>
@@ -156,7 +299,7 @@ const SingleProduct = () => {
                         </div>
                     </div>
                 </div>
-            </div>
+            </section>
 
             <section className="popular-wrapper home-wrapper-2 py-5">
                 <div className="container-xxl">
