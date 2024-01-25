9cdec19575e05ac4ea7e46557ea5803fd901af07
ramziddin-frontent <ramziddintursunov82@gmail.com>
2024-01-25 21:43:56 +0500

    add wishlist

--- ui/src/pages/Wishlist.js
+++ ui/src/pages/Wishlist.js
@@ -1,89 +1,64 @@
-import React from 'react';
 import Meta from "../components/Meta";
 import BreadCrumb from "../components/BreadCrumb";
-import {RxCross1} from "react-icons/rx";
+import { RxCross1 } from "react-icons/rx";
+import { addWishlist } from "../features/product/productSlice";
+import { getUserProductWishlist, resetState } from "../features/auth/userSlice";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 const Wishlist = () => {
-    return (
-        <>
-            <Meta title={"Wishlist"}/>
-            <BreadCrumb title={"Wishlist"}/>
-            <div className="wishlist-wrapper home-wrapper-2 py-5">
-                <div className="container-xxl">
-                    <div className="row">
-                        <div className="col-3">
-                            <div className="wishlist-card position-relative">
-                                <RxCross1 className="position-absolute cross img-fluid" size={35}/>
-                                <div className="wishlist-card-image">
-                                    <img src="https://m.media-amazon.com/images/I/71ulah9iIwL._AC_SL1500_.jpg"
-                                         alt="image"/>
-                                </div>
-                                <h5 className="title">
-                                    Apple Watch Series 8 [GPS 41mm] Smart Watch w/Midnight Aluminum Case with
-                                    Midnight Sport Band - M/L. Fitness Tracker, Blood Oxygen & ECG Apps, Always-On
-                                    Retina Display, Water Resistant
-                                </h5>
-                                <h6 className="price">
-                                    $364.89
-                                </h6>
-                            </div>
-                        </div>
-                        <div className="col-3">
-                            <div className="wishlist-card position-relative">
-                                <RxCross1 className="position-absolute cross img-fluid" size={35}/>
-                                <div className="wishlist-card-image">
-                                    <img src="https://m.media-amazon.com/images/I/71ulah9iIwL._AC_SL1500_.jpg"
-                                         alt="image"/>
-                                </div>
-                                <h5 className="title">
-                                    Apple Watch Series 8 [GPS 41mm] Smart Watch w/Midnight Aluminum Case with
-                                    Midnight Sport Band - M/L. Fitness Tracker, Blood Oxygen & ECG Apps, Always-On
-                                    Retina Display, Water Resistant
-                                </h5>
-                                <h6 className="price">
-                                    $364.89
-                                </h6>
-                            </div>
-                        </div>
-                        <div className="col-3">
-                            <div className="wishlist-card position-relative">
-                                <RxCross1 className="position-absolute cross img-fluid" size={35}/>
-                                <div className="wishlist-card-image">
-                                    <img src="https://m.media-amazon.com/images/I/71ulah9iIwL._AC_SL1500_.jpg"
-                                         alt="image"/>
-                                </div>
-                                <h5 className="title">
-                                    Apple Watch Series 8 [GPS 41mm] Smart Watch w/Midnight Aluminum Case with
-                                    Midnight Sport Band - M/L. Fitness Tracker, Blood Oxygen & ECG Apps, Always-On
-                                    Retina Display, Water Resistant
-                                </h5>
-                                <h6 className="price">
-                                    $364.89
-                                </h6>
-                            </div>
-                        </div>
-                        <div className="col-3">
-                            <div className="wishlist-card position-relative">
-                                <RxCross1 className="position-absolute cross img-fluid" size={35}/>
-                                <div className="wishlist-card-image">
-                                    <img src="https://m.media-amazon.com/images/I/71ulah9iIwL._AC_SL1500_.jpg"
-                                         alt="image"/>
-                                </div>
-                                <h5 className="title">
-                                    Apple Watch Series 8 [GPS 41mm] Smart Watch w/Midnight Aluminum Case with
-                                    Midnight Sport Band - M/L. Fitness Tracker, Blood Oxygen & ECG Apps, Always-On
-                                    Retina Display, Water Resistant
-                                </h5>
-                                <h6 className="price">
-                                    $364.89
-                                </h6>
-                            </div>
-                        </div>
+  const dispatch = useDispatch();
+
+  const removeToWishlist = (id) => {
+    dispatch(addWishlist(id));
+    setTimeout(() => {
+        getWishlistFromDb()
+    }, 300);
+  };
+
+  useEffect(() => {
+        dispatch(resetState());
+    getWishlistFromDb()
+  }, []);
+
+  const getWishlistFromDb = () => {
+    dispatch(getUserProductWishlist());
+  };
+  const wishlistState = useSelector(state => state?.auth.wishlist.wishlist);
+  return (
+    <>
+      <Meta title={"Wishlist"} />
+      <BreadCrumb title={"Wishlist"} />
+      <div className="wishlist-wrapper home-wrapper-2 py-5">
+        <div className="container-xxl">
+          <div className="row">
+            {wishlistState?.map((item, index) => {
+              return (
+                <>
+                  <div className="col-3" key={index}>
+                    <div className="wishlist-card position-relative">
+                      <RxCross1
+                        className="position-absolute cross img-fluid"
+                        size={35}
+                        onClick={() => {
+                          removeToWishlist(item?._id);
+                        }}
+                      />
+                      <div className="wishlist-card-image">
+                        <img src={item?.images[0]?.url} alt="image" />
+                      </div>
+                      <h5 className="title">{item?.title}</h5>
+                      <h6 className="price">$ {item?.price}</h6>
                     </div>
-                </div>
-            </div>
-        </>
-    );
+                  </div>
+                </>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Wishlist;
