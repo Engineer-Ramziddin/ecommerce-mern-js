97ef23ca82c7b581bf0c46f1718e7bd3fe141fa2
ramziddin-frontent <ramziddintursunov82@gmail.com>
2024-01-25 21:49:06 +0500

    add product list delete button

--- admin/src/pages/Productlist.js
+++ admin/src/pages/Productlist.js
@@ -1,10 +1,12 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Table } from "antd";
 import { BiEdit } from "react-icons/bi";
 import { AiFillDelete } from "react-icons/ai";
 import { useDispatch, useSelector } from "react-redux";
-import { getProducts } from "../features/product/productSlice";
+import { getProducts, resetState, deleteAProduct } from "../features/product/productSlice";
 import { Link } from "react-router-dom";
+import CustomModal from "../components/CustomModal";
+
 const columns = [
   {
     title: "SNo",
@@ -40,9 +42,22 @@ const columns = [
   },
 ];
 
+
+
 const Productlist = () => {
+  const [open, setOpen] = useState(false);
+  const [productId, setproductId] = useState("");
+  const showModal = (e) => {
+    setOpen(true);
+    setproductId(e);
+  };
+
+  const hideModal = () => {
+    setOpen(false);
+  };
   const dispatch = useDispatch();
   useEffect(() => {
+     dispatch(resetState());
     dispatch(getProducts());
   }, []);
   const productState = useSelector((state) => state.product.products);
@@ -57,22 +72,42 @@ const Productlist = () => {
       price: `${productState[i].price}`,
       action: (
         <>
-          <Link to="/" className=" fs-3 text-danger">
+          <Link to={`/admin/product/`} className=" fs-3 text-danger">
             <BiEdit />
           </Link>
-          <Link className="ms-3 fs-3 text-danger" to="/">
+          <button
+            className="ms-3 fs-3 text-danger bg-transparent border-0"
+            onClick={() => showModal(productState[i]._id)}
+          >
             <AiFillDelete />
-          </Link>
+          </button>
         </>
       ),
     });
   }
+
+  const deleteProduct = (e) => {
+    dispatch(deleteAProduct(e));
+
+    setOpen(false);
+    setTimeout(() => {
+      dispatch(getProducts());
+    }, 100);
+  };
   return (
     <div>
       <h3 className="mb-4 title">Products</h3>
       <div>
         <Table columns={columns} dataSource={data1} />
       </div>
+      <CustomModal
+        hideModal={hideModal}
+        open={open}
+        performAction={() => {
+          deleteProduct(productId);
+        }}
+        title="Are you sure you want to delete this Product?"
+      />
     </div>
   );
 };
