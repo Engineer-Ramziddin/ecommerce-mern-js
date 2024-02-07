41a57e015b7814a4bf2d62b6254892793f39556d
ramziddin-frontent <ramziddintursunov82@gmail.com>
2024-02-07 15:27:11 +0500

    add a dynamic blog card

--- ui/src/components/BlogCard.js
+++ ui/src/components/BlogCard.js
@@ -1,38 +1,39 @@
 import React from 'react';
 import {Link} from "react-router-dom";
-import laptop from "../assets/images/laptop.jpg";
-const BlogCard = () => {
+
+const truncateText = (text, numWords) => {
+  const words = text.split(" ");
+  if (words.length > numWords) {
+    return words.slice(0, numWords).join(" ") + "...";
+  }
+  return text;
+};
+const BlogCard = (props) => {
+  const { id, title, description, images, date } = props;
+  const truncatedDescription = truncateText(description, 20);
     return (
-        <>
-            <div className="blog-card gr-6">
-                <div className="card-image">
-                    <img className="img-fluid w-100"
-                         src={laptop} alt=""/>
-                </div>
-                <div className="blog-content">
-                    <p className="date">3 Jan, 2024</p>
-                    <h5 className="title">
-                        <h2 className="article-card__title 123" id="Article-558559887529">
-                            A beatiful sunday morning renaissance
-                        </h2>
-                    </h5>
-                    <p className="desc">
-                        Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of
-                        classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a
-                        Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin
-                        words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in
-                        classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32
-                        and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero,
-                        written in 45 BC. This book is a treatise on the theory of ethics, very popular during the
-                        Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in
-                        section 1.10.32.
-                    </p>
-                    <Link to="/blog/:id" className="button">
-                        Read more
-                    </Link>
-                </div>
-            </div>
-        </>
+      <>
+        <div className="blog-card gr-6">
+          <div className="card-image">
+            <img className="img-fluid w-100" src={images} alt="" />
+          </div>
+          <div className="blog-content">
+            <p className="date">{date}</p>
+            <h5 className="title">
+              <h2 className="article-card__title 123" id="Article-558559887529">
+                {title}
+              </h2>
+            </h5>
+            <p
+              className="desc"
+              dangerouslySetInnerHTML={{ __html: truncatedDescription }}
+            ></p>
+            <Link to={`/blog/${id}`} className="button">
+              Read more
+            </Link>
+          </div>
+        </div>
+      </>
     );
 };
 
